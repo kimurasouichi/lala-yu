a822785af19a1797c7ff0584a7c5450965886550
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-10-08 06:42:44 +0900

    viewsの設置　各リンクの記述

---
+++ app/controllers/cookings_controller.rb
@@ -0,0 +1,5 @@
+class CookingsController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "lalas#index"
-  resources :lalas, only: [:index] do
+  resources :lalas, only: [:index, :new] do
   end
   resources :infos, only: [:index ] do
+  end
+  resources :cookings, only: [:index]do
+  end
+  resources :buildings, only: [:index]do
   end
 end
