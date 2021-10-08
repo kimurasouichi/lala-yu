8c495d51c4e5c18228e94a445047f2ce6078289f
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-10-08 18:54:49 +0900

    重要　railsでjavascriptを使用するときには上記をコメントアウトする

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,13 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("../top_page")
+// require("../info_page")
+// require("../cooking_page")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
